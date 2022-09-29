5b0039007a9ded4a6f0e700f57e862a6700c91ed
asw-g-008 <asw-g-008@asw-g-008noMac-mini.local>
2022-09-30 00:52:11 +0900

    jinjaのパターン作成

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ env = Environment(loader=FileSystemLoader('./template/', encoding='utf8'))
 tmpl = env.get_template('template.j2')
 
 # エクセルファイル読み込み
-params = {"name": "田中", "a_value": "test1", "b_value": "test2"}
+params = {"name": "田中",
+          "a_value": "test1", "b_value": "test2",
+          'items': ['みかん', 'りんご', 'パナナ'],
+          'dataObj': {'a': '愛', 'b': '雨', 'c': '江尾'}, }
 
 # レンダリングして出力
 rendered_s = tmpl.render(params)
